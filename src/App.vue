27d3c2a920f3288a752bf597030b8f35afcfeef8
zak771 <zakispoupah@gmail.com>
2022-05-07 15:40:55 +0100

    fixed the main window maximizable issue

--- src/App.vue
+++ src/App.vue
@@ -13,58 +13,55 @@
 <script>
 import { ipcRenderer } from "electron";
 export default {
-	components: {
+	components: {},
+	data() {
+		return {};
 	},
-  data() {
-		return {
-		};
-  },
 	methods: {
 		minimize() {
-			ipcRenderer.send('minimize')
+			ipcRenderer.send("minimize");
 		},
 		maximize() {
-			ipcRenderer.send("maximize")
+			ipcRenderer.send("maximize");
 		},
 		close() {
-			localStorage.removeItem("user-info")
-			ipcRenderer.send("close")
+			localStorage.removeItem("user-info");
+			ipcRenderer.send("close");
+		},
+	},
+	beforeCreate() {
+		let user = localStorage.getItem("user-info");
+		if (!user) {
+			this.$router.push({ name: "signIn" });
+		} else {
+			this.$router.push({ name: "home" });
 		}
 	},
-  beforeCreate() {
-    let user = localStorage.getItem("user-info");
-    if (!user) {
-      this.$router.push({ name: "signIn" });
-    } else {
-      this.$router.push({ name: "home" });
-    }
-  },
-	created() {
-	},
+	created() {},
 	mounted() {
 		this.$store.dispatch("fetching_data");
-	}
+	},
 };
 </script>
 
 <style>
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  width: 100%;
-  color: #282c40;
-  height: 100vh;
-  background-color: #f7f7f7;
-  /*display: flex;*/
-  /*align-items: center;*/
-  /*justify-content: center;*/
-  position: relative;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	width: 100%;
+	color: #282c40;
+	height: 100vh;
+	background-color: #f7f7f7;
+	/*display: flex;*/
+	/*align-items: center;*/
+	/*justify-content: center;*/
+	position: relative;
 }
 .titleBar {
 	width: 100%;
@@ -78,69 +75,69 @@ export default {
 	-webkit-app-region: drag;
 }
 .wc-box {
-  position: absolute;
+	position: absolute;
 	right: 10px;
 	-webkit-app-region: no-drag;
 }
 
 .wc-box div {
-  position: relative;
-  float: left;
-  width: 20px;
-  height: 20px;
+	position: relative;
+	float: left;
+	width: 20px;
+	height: 20px;
 	margin-left: 5px;
 }
 
 .wc-box div:before,
 .wc-box div:after {
-  top: 30%;
-  right: 30%;
-  bottom: 30%;
-  left: 30%;
-  content: " ";
-  position: absolute;
-  border-color: #CCCCCC;
-  border-style: solid;
-  border-width: 0 0 2px 0;
+	top: 30%;
+	right: 30%;
+	bottom: 30%;
+	left: 30%;
+	content: " ";
+	position: absolute;
+	border-color: #cccccc;
+	border-style: solid;
+	border-width: 0 0 2px 0;
 }
 
 .wc-box .minimize:before {
-  border-bottom-width: 2px;
+	border-bottom-width: 2px;
 }
 
 .wc-box .maximize:before {
-  border-width: 1px 1px 2px 1px;
+	border-width: 1px 1px 2px 1px;
 }
 
 .wc-box .close:before,
 .wc-box .close:after {
-  bottom: 50%;
-  top: 50%;
+	bottom: 50%;
+	top: 50%;
 }
 
 .wc-box .close:before {
-  transform: rotate(45deg);
+	transform: rotate(45deg);
 }
 
 .wc-box .close:after {
-  transform: rotate(-45deg);
+	transform: rotate(-45deg);
 }
 
 .wc-box div:hover {
-  background-color: #CCCCCC;
+	background-color: #cccccc;
 }
 
 .wc-box .close:hover {
-  background-color: #E04343;
+	background-color: #e04343;
 }
 
 .wc-box div:hover:after,
 .wc-box div:hover:before {
-  border-color: #333333;
+	border-color: #333333;
 }
 
 .wc-box .close:hover:after,
 .wc-box .close:hover:before {
-  border-color: #FFFFFF;
+	border-color: #ffffff;
 }
 </style>
